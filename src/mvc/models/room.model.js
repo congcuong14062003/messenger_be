f30e4d040ec5b860935136c4025fab5702961149
cuongkoi <cuongkoi1406@gmail.com>
2024-03-02 19:29:38 +0700

    ahihi

--- src/mvc/models/room.model.js
+++ src/mvc/models/room.model.js
@@ -1,6 +1,6 @@
 import pool from "../../configs/database/cnDB";
 class Room {
-    constructor(room) {
+    constructOR(room) {
         this.room_id = room.room_id;
         this.room_name = room.room_name || null;
     }
@@ -20,8 +20,8 @@ class Room {
             } else {
                 return null;
             }
-        } catch (error) {
-            console.error('Error getting room by members:', error);
+        } catch (errOR) {
+            console.errOR('ErrOR getting room by members:', errOR);
             return null;
         }
     }
@@ -29,11 +29,11 @@ class Room {
     static async getRoomByIDUser(sender_id, receiver_id) {
         try {
             // Tạo một room mới với room_name null và room_id tự tăng
-            const query = `select room_id from PrivateMessages where sender_id = ${sender_id} and receiver_id = ${receiver_id} or sender_id = ${receiver_id} and receiver_id = ${sender_id} group by room_id`;
+            const query = `SELECT room_id FROM PrivateMessages WHERE sender_id = ${sender_id} AND receiver_id = ${receiver_id} OR sender_id = ${receiver_id} AND receiver_id = ${sender_id} GROUP BY room_id`;
             const [result] = await pool.query(query);
             return result[0].room_id ?? null;
-        } catch (error) {
-            console.error('Error creating new room:', error);
+        } catch (errOR) {
+            console.errOR('ErrOR creating new room:', errOR);
             return null;
         }
     }
@@ -45,8 +45,8 @@ class Room {
             const [result] = await pool.query(query);
             const room_id = result.insertId;
             return new Room({ room_id: room_id });
-        } catch (error) {
-            console.error('Error creating new room:', error);
+        } catch (errOR) {
+            console.errOR('ErrOR creating new room:', errOR);
             return null;
         }
     }
